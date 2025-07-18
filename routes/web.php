087f599e6c0e85daa743d4815e655d696533d018
edmarcDavey <devhehytbave@gmail.com>
2025-07-19 03:33:23 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
     return Inertia::render('BantayBantay');
 });
 
